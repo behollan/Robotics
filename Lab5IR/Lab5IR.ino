578e890e7ab3685e9d35ff736b98a63cd64fc2dc
Ben Holland <benjamin.holland1@gmail.com>
2016-11-17 00:32:16 -0700

    Edits as of 11/17/16
    Added maze exploration, bug still happening with solving

--- Lab5IR/Lab5IR.ino
+++ Lab5IR/Lab5IR.ino
@@ -1,59 +1,142 @@
+#include "MotorControl.h"
+#include "PinChangeInt.h"
+
 #define LSense 0
 #define FSense 1
 #define startPin 9
+#define EncoderMotorLeft	7
+#define EncoderMotorRight	8
+
+//     Variables
+volatile unsigned int leftEncoderCount = 0;
+volatile unsigned int rightEncoderCount = 0;
+int countsDesired, countsCompleted, CMDleft, CMDright, errorLeft, errorRight, countLeft, countRight;
+int inittime = millis();
+int rate = 50;
+int correctedPWMLeft = 190;
+int nodesToMM = 30;
 
 int LSenseVal, FSenseVal;
 int LTol = 162; //15 cm
 int FTol = 278; //8 cm
-int wallDist = 372; //5cm
-int correctionAngle = 20;
+int wallDist = 450; //5cm
 
-#include "MotorControl.h"
+#define startNode 21 //Start Location
+#define endNode 24 //End location
+
+int buttonCount;
+String cardDir[50] = { "N","E","S","W", "N","E","S","W", "N","E","S","W", "N","E","S","W", "N","E","S","W", "N","E","S","W"};
+int curDir = 13; //Current direction tracker, start facing east
+int currentNode = startNode;
+
+String directions[50] = {};
 
 
 void setup()
 {
 	pinMode(startPin, INPUT_PULLUP);
-	Serial.begin(9600);
+	Serial.begin(115200);
+
+	pinMode(EncoderMotorLeft, INPUT_PULLUP);     //set the pin to input pullup
+	attachPinChangeInterrupt(EncoderMotorLeft, indexLeftEncoderCount, CHANGE);
+
+	pinMode(EncoderMotorRight, INPUT_PULLUP);     //set the pin to input pullup
+	attachPinChangeInterrupt(EncoderMotorRight, indexRightEncoderCount, CHANGE);
 }
+
 
 void loop()
 {
-		LSenseVal = analogRead(LSense);
+	int buttonState = digitalRead(startPin);
+	if (buttonState == 0) {
+		buttonCount++;
+		Serial.print("Button counter: ");
+		Serial.println(buttonCount);
+		if (buttonCount == 1) {
+			exploreMaze();
+	}
+		if (buttonCount >= 2) {
+			runMaze();
+		}
+	}
+}
+
+void exploreMaze() {
+	while (currentNode != endNode) {
 		FSenseVal = analogRead(FSense);
-		Serial.print("Left Sensor: ");
-		Serial.println(LSenseVal);
-		Serial.print("RightSensor: ");
-		Serial.println(FSenseVal);
-		/*	LSenseVal = map(LSenseVal, 0, 100, 0, 1023);
-			FSenseVal = map(FSenseVal, 0, 100, 0, 1023);
-			*/
-		if (LSenseVal < LTol) {
-			delay(300);
-			MC.ForwardStop();
-			MC.Left(90);
-			MC.LeftStop();
-			MC.Forward(150);
-			delay(1000);
+		LSenseVal = analogRead(LSense);
 
+		Serial.print("Current direction: ");
+		Serial.println(cardDir[curDir]);
+		Serial.print("Current node: ");
+		Serial.println(currentNode);
+		if (FSenseVal < FTol) {
+			driveDir("F");
+			nodeNumber(cardDir[curDir]);
+			computeNewCardDir("F");
 		}
-		else if (FSenseVal > FTol) {
-			MC.ForwardStop();
-			MC.Right(90);
-			MC.RightStop();
+		else if (LSenseVal < LTol) {
+			driveDir("L");
+			nodeNumber(cardDir[curDir]);
+			computeNewCardDir("L");
 		}
-		else if (LSenseVal > wallDist) {
-			MC.ForwardStop();
-			MC.Right(correctionAngle);
-			MC.RightStop();
-			MC.Forward(150);
-			delay(100);
-		}
-
 		else {
-			MC.Forward(150);
+			driveDir("R");
+			nodeNumber(cardDir[curDir]);
+			computeNewCardDir("R");
 		}
-
+	}
+	Serial.println("Maze Solve Complete!");
+}
+void runMaze() {
 
 }
 
+void computeNewCardDir(String dir) {
+	if (dir == "F") {
+	}
+	if (dir == "L") {
+		curDir -= 1;
+	}
+	if (dir == "R") {
+		curDir += 1;
+	}
+}
+
+void nodeNumber(String cardDir) {
+	if (cardDir == "N") {
+		currentNode -= 5;
+	}
+	else if (cardDir == "E") {
+		currentNode += 1;
+	}
+	else if (cardDir == "S") {
+		currentNode += 5;
+	}
+	else if (cardDir == "W") {
+		currentNode -= 1;
+	}
+}
+
+void driveDir(String dir) {
+	if (dir == "F") {
+		MC.Forward(1);
+	}
+	else if (dir == "L") {
+		MC.Left(90);
+		MC.Forward(1);
+	}
+	else if (dir == "R") {
+		MC.Right(90);
+		MC.Forward(1);
+	}
+}
+
+void indexLeftEncoderCount()
+{
+	leftEncoderCount++;
+}
+void indexRightEncoderCount()
+{
+	rightEncoderCount++;
+}
